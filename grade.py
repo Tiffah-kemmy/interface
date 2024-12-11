52ed87ca41bf34917b559ec5c38fe18c84188d0c
Tiffah-kemmy <latifakaumo8@gmail.com>
2024-12-11 18:03:05 +0300

    add the grade.py file

---
+++ grade.py
@@ -0,0 +1,8 @@
+grades = [90,88,77,97,68,72]
+for score in grades :
+    if score >=90:
+        print("The grade is A")
+    elif score >=80:
+        print ("The grade is B")
+    else:
+        print("The grade is C")
